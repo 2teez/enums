6114d62bcc88fc0608eafb1db4533ccdd31ffe88
timothy adigun <adigun.timothy@gmail.com>
2025-06-21 14:06:50 +0100

    moved the nums crates tests to it's folder

--- src/nums.rs
+++ src/nums.rs
@@ -46,37 +46,3 @@ impl<T: Clone + std::fmt::Debug, R: Clone + std::fmt::Debug> Partition<T, R> for
 
 #[cfg(test)]
 mod tests;
-/*{
-    use arraylist::arl::ArrayList;
-    use arraylist::arraylist;
-
-    use super::*;
-
-    #[test]
-    fn test_indexes_for_a_list() {
-        let lists = arraylist![4, 2, 0].to_vec();
-        assert_eq!(lists.nums(), [0, 1, 2])
-    }
-
-    #[test]
-    fn test_indexes_for_a_list_using_customized_index() {
-        let lists = arraylist!["cat", "lion", "bear"].to_vec();
-        assert_eq!(
-            lists.nums_starting_at(5.into()),
-            [5, 6, 7],
-            "should be equal."
-        )
-    }
-
-    #[test]
-    fn test_tuple_first_value() {
-        let lists = arraylist!["cat", "lion", "bear"].to_vec().enums();
-        assert_eq!(lists[0].0, lists[0].first())
-    }
-
-    #[test]
-    fn test_tuple_second_value() {
-        let lists = arraylist!["cat", "lion", "bear"].to_vec().enums();
-        assert_eq!(lists[2].1, lists[2].second())
-    }
-}*/
